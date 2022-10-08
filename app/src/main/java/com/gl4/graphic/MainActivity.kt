7138e49fd7b006a02f54875e15665c1886a025e4
BorchaniSalma <sborcheni@ieee.org>
2022-10-08 14:05:37 +0100

    main activity

---
+++ app/src/main/java/com/gl4/graphic/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.gl4.graphic
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var txtEmail: EditText
+    lateinit var txtPassword : EditText
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txtEmail = findViewById(R.id.editTextEmail)
+        txtPassword = findViewById(R.id.editPassword)
+
+    }
+
+    fun login(v: View?) {
+        var email = txtEmail.text.toString()
+        var password = txtEmail.text.toString()
+
+        if( v?.id == R.id.btnLogin){
+            if (email.equals("login")  && password.equals("login")){
+                val toast = Toast.makeText(applicationContext, "Les données sont correctes", Toast.LENGTH_LONG)
+                toast.show()
+                intent.putExtra("email",email)
+                startActivity(intent)
+            }else{
+                val toast = Toast.makeText(applicationContext, "Les données sont incorrectes", Toast.LENGTH_LONG)
+                toast.show()
+            }
+        }
+    }
+    }
